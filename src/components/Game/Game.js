ec1e9aafe25504b288b86b388e8bd8c8b19d0d83
cdirks4 <cdirks4@me.com>
2022-08-06 17:39:13 -0400

    Game.js inline css updated

--- src/components/Game/Game.js
+++ src/components/Game/Game.js
@@ -4,18 +4,34 @@ import { Button, Form } from 'react-bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 const Game = ({ address, balance }) => {
 	return (
-		<div style={{ display: 'grid', textAlign: 'center' }}>
-			<h1>ROCK, Paper, Scissors</h1>
+		<div style={{ display: 'grid', textAlign: 'center', paddingTop: '16vh' }}>
+			<h1 style={{ fontSize: '40px' }}>Rock, Paper, Scissors</h1>
 			<Form>
 				<Form.Group className='mb-3' controlId='formBasicEmail'>
-					<Form.Label>Are you a Deployer or Attacher?</Form.Label>
+					<Form.Label style={{ paddingTop: '2vh', fontSize: '20px' }}>
+						Are you a Deployer or an Attacher?
+					</Form.Label>
 				</Form.Group>
-				<div style={{ display: 'flex', justifyContent: 'space-evenly' }}>
-					<Link to='/deployer'>
-						<Button variant='danger'>Deployer</Button>
-					</Link>
+				<div
+					style={{
+						display: 'flex',
+						justifyContent: 'space-evenly',
+						paddingTop: '4vh',
+					}}>
 					<Link to='/attacher'>
-						<Button variant='success'>Attacher</Button>
+						<Button
+							variant='danger'
+							className='game-button'
+							style={{
+								marginLeft: '6w',
+							}}>
+							Deployer
+						</Button>
+					</Link>
+					<Link to='/deployer'>
+						<Button variant='success' style={{ marginRight: '6w' }}>
+							Attacher
+						</Button>
 					</Link>
 				</div>
 			</Form>
